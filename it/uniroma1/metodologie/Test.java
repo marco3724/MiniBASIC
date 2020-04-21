efa8938c62853f8d1c1f5c706a8b399e9a663653
marco <marcowang05@gmail.com>
2020-04-21 19:10:20 +0200

    consegnato

--- it/uniroma1/metodologie/Test.java
+++ it/uniroma1/metodologie/Test.java
@@ -2,50 +2,37 @@ package it.uniroma1.metodologie;
 
 import eccezioni.OperatoreNonTrovatoException;
 import eccezioni.TipiIncopamtibiliException;
-import espressioni.*;
+import espressioni.EspressioneConfronto.Operatore;
 import espressioni.Variabile.Nome;
-import istruzioni.*;
+import istruzioni.Print;;
 
 public class Test {
 	public static void main(String[] args)  {
 		try {
-			Variabile v1 = new Variabile(Nome.$0,0);
-			Variabile v2 = new Variabile(Nome.$0,0);
-			Variabile v3 = new Variabile(Nome.$0,0);
-			Variabile v4 = new Variabile(Nome.$0,0);
-			Variabile v15 = new Variabile(Nome.$0,0);
-			Variabile v16 = new Variabile(Nome.$0,0);
-			Variabile v17 = new Variabile(Nome.$0,0);
-			Variabile v18 = new Variabile(Nome.$0,0);
-			Variabile v19 = new Variabile(Nome.$0,0);
-			Variabile v10 = new Variabile(Nome.$0,0);
-			//Variabile v102 = new Variabile(Nome.$0,0);
-			
-	
-		Programma p1 = Programma.fromFile("prg/28/fibonacci.bas");
-		Programma p2 = Programma.fromFile("prg/full/pari_dispari.bas");
-		Programma p3 = Programma.fromFile("prg/full/prg1.bas");
-		Programma p4 = Programma.fromFile("prg/full/fibonacci.bas");
-		Programma p5 = Programma.fromFile("prg/full/ordina.bas");
-		Programma p6 = Programma.fromFile("prg/28/successione.bas");
-		Programma p7 = Programma.fromFile("prova.txt");
-		new MiniBASIC().esegui(p7);
+			Programma p1 = Programma.fromFile("prg/28/fibonacci.bas");
+			Programma p2 = Programma.fromFile("prg/full/pari_dispari.bas");
+			Programma p3 = Programma.fromFile("prg/full/prg1.bas");
+			Programma p4 = Programma.fromFile("prg/full/fibonacci.bas");
+			Programma p5 = Programma.fromFile("prg/full/ordina.bas");
+			Programma p6 = Programma.fromFile("prg/28/successione.bas");
+
+			System.out.println("1 test");
+			new MiniBASIC().esegui(p1);
+			System.out.println("\n2 test");
+			new MiniBASIC().esegui(p2);
+			System.out.println("\n3 test");
+			new MiniBASIC().esegui(p3);
+			System.out.println("\n4 test");
+			new MiniBASIC().esegui(p4);
+			System.out.println("\n5 test");
+			new MiniBASIC().esegui(p5);
+			System.out.println("\n6 test");
+			new MiniBASIC().esegui(p6);
+		
 		}
 		catch(Exception e) {System.out.println(e);}
-/*
-		new MiniBASIC().esegui(p1);//
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");*/
-		
-		//new MiniBASIC().esegui(p3);
-		/*
-		//new MiniBASIC().esegui(p4);
-		//new MiniBASIC().esegui(p5);//
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		new MiniBASIC().esegui(p6);//*/
+
+
 				
 
 	}
